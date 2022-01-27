48395155189969e8078fefb0a6909425539e11d1
Jason Santos <jslum18@hotmail.com>
2022-01-27 16:56:14 -0500

    popup for resume

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 $(() => {
+
+////////////////////////////////////////////////////////////////////////////
+// CAROUSEL
+////////////////////////////////////////////////////////////////////////////
   currentImgIndex = 0;
   numOfImages = $('.carousel_track').children().length -1
+
+  // select carousel button when clicked on
   $('.carousel_button-right').on('click', () => {
-    // stuff will go here
+    // find the children of Ul carousel_track and hide current image
     $('.carousel_track').children().eq(currentImgIndex).css('display', 'none')
+    // if number of images is less than the current image
     if (currentImgIndex < numOfImages) {
+      // add 1 to currentImgIndex
       currentImgIndex++
     } else {
       currentImgIndex = 0
@@ -21,4 +29,26 @@ $(() => {
     }
     $('.carousel_track').children().eq(currentImgIndex).css('display', 'block')
   })
+
+  ////////////////////////////////////////////////////////////////////////////
+  // CAROUSEL
+  ////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+  // RESUME POP UP
+  ////////////////////////////////////////////////////////////////////////////
+// show popup
+  $('#show-popup-button').click(() => {
+    $('#popup-container').show()
+  })
+
+// close pop up
+$('#close-btn').click(() => {
+  $('#popup-container').hide( )
+})
+////////////////////////////////////////////////////////////////////////////
+// RESUME POP UP
+////////////////////////////////////////////////////////////////////////////
+
+
+
 });
