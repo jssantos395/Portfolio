7b8d05f87fcc829ac6d18e904b646c58868b76fe
Jason Santos <jslum18@hotmail.com>
2022-01-26 19:57:57 -0500

    got carousel to work

---
+++ app.js
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////
+// carousel
+////////////////////////////////////////////////////////////
+
+$(() => {
+  currentImgIndex = 0;
+  numOfImages = $('.carousel_track').children().length -1
+  $('.carousel_button-right').on('click', () => {
+    // stuff will go here
+    $('.carousel_track').children().eq(currentImgIndex).css('display', 'none')
+    if (currentImgIndex < numOfImages) {
+      currentImgIndex++
+    } else {
+      currentImgIndex = 0
+    }
+    console.log(currentImgIndex);
+    $('.carousel_track').children().eq(currentImgIndex).css('display', 'block')
+  })
+  $('.carousel_button-left').on('click', () => {
+    $('.carousel_track').children().eq(currentImgIndex).css('display', 'none')
+    if (currentImgIndex > 0) {
+      currentImgIndex--
+    } else {
+      currentImgIndex = numOfImages
+    }
+    $('.carousel_track').children().eq(currentImgIndex).css('display', 'block')
+  })
+});
